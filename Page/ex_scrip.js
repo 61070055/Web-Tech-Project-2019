e27dd129c8af0a31771cb4adb0131dc4c3e5f2fd
61070055 <61070055@kmitl.ac.th>
2019-11-12 11:19:19 +0700

    ex update

--- Page/ex_scrip.js
+++ Page/ex_scrip.js
@@ -1,5 +1,5 @@
 const progress = document.getElementById("progress");
-
+let runner = 0;
 
     for (let qIndex = 0; qIndex <= 7; qIndex++) {
         progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
